f833556ffff45c30bdb5c136abba58d6a21ca9ee
Lesair Valmont <ixra.net@gmail.com>
2020-04-15 18:48:12 -0500

    [AB#282] Using new routing stub pages.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import Index from "./pages/index";
+import SignIn from "./pages/signIn";
+import NotFound from "./pages/notFound";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Index} />
+        <Route path="/signIn" component={SignIn} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
+);
 
+ReactDOM.render(routing, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
