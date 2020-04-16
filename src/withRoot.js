0e8efc22e25dffb2007477baee38aaf52e4f5158
Lesair Valmont <ixra.net@gmail.com>
2020-04-16 18:55:05 -0500

    [AB#286] Added withRoot HOC. For managing and applying themes.

---
+++ src/withRoot.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import deepPurple from "@material-ui/core/colors/deepPurple";
+import deepOrange from "@material-ui/core/colors/deepOrange";
+import red from "@material-ui/core/colors/red";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+// A theme with custom primary and secondary color.
+// It's optional.
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: deepPurple,
+    secondary: deepOrange,
+    error: red
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
+
+function withRoot(Component) {
+  function WithRoot(props) {
+    // MuiThemeProvider makes the theme available down the React tree
+    // thanks to React context.
+    return (
+      <MuiThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...props} />
+      </MuiThemeProvider>
+    );
+  }
+
+  return WithRoot;
+}
+
+export default withRoot;
