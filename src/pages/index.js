aa3698f26a558f6ecd76e3aa91ebda089a7b4ed0
Lesair Valmont <ixra.net@gmail.com>
2020-04-16 18:55:55 -0500

    [AB#286] Testing Material-UI. Making sure Material-UI and theme are working correctly.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import withRoot from "../withRoot";
+import Typography from "@material-ui/core/Typography";
+
+const styles = (theme) => ({
+  root: {
+    height: "100vh",
+  },
+});
 
 class Index extends React.Component {
   render() {
-    return <p>Index page.</p>;
+    return (
+      <React.Fragment>
+        <Typography>Index page.</Typography>
+      </React.Fragment>
+    );
   }
 }
 
-export default Index;
+Index.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withRoot(withStyles(styles)(Index));
