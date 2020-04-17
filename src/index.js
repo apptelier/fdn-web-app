b5dc83c8ecc1bfddb82450a5e15926a8234d04e3
Lesair Valmont <ixra.net@gmail.com>
2020-04-17 16:54:13 -0500

    [AB#291] Use withBars HOC in index routes.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
+import WithBars from "./pages/withBars";
 import Index from "./pages/index";
 import SignIn from "./pages/signIn";
 import NotFound from "./pages/notFound";
@@ -11,7 +12,14 @@ const routing = (
   <Router>
     <div>
       <Switch>
-        <Route exact path="/" component={Index} />
+        <Route
+          exact
+          path="/"
+          component={WithBars(Index, {
+            attendanceSummary: "visible",
+            clock: "visible",
+          })}
+        />
         <Route path="/signIn" component={SignIn} />
         <Route component={NotFound} />
       </Switch>
