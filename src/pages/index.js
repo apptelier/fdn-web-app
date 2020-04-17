1513e2fc51622c072d6ecbd04ab3ae1bfde3d69c
Lesair Valmont <ixra.net@gmail.com>
2020-04-17 16:52:43 -0500

    [AB#290] Main index dashboard.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,61 @@
 import React from "react";
+import clsx from "clsx";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-import withRoot from "../withRoot";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 
 const styles = (theme) => ({
-  root: {
-    height: "100vh",
+  paper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column",
+  },
+  fixedHeight: {
+    height: 240,
   },
 });
 
 class Index extends React.Component {
+  static pageDisplayName = "Dashboard";
+
   render() {
+    const { classes } = this.props;
+    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
     return (
-      <React.Fragment>
-        <Typography>Index page.</Typography>
-      </React.Fragment>
+      <Grid container spacing={3}>
+        {/* Chart */}
+        <Grid
+          item
+          xs={12}
+          md={8}
+          lg={9}
+          hidden={this.props.attendanceSummary !== "visible"}
+        >
+          <Paper className={fixedHeightPaper}>
+            <Typography>Sample dashboard layout.</Typography>
+          </Paper>
+        </Grid>
+        {/* Recent Deposits */}
+        <Grid
+          item
+          xs={12}
+          md={4}
+          lg={3}
+          hidden={this.props.clock !== "visible"}
+        >
+          <Paper className={fixedHeightPaper}>
+            <Typography>{new Date().toTimeString().split(" ")[0]}</Typography>
+          </Paper>
+        </Grid>
+        {/* Recent Orders */}
+        <Grid item xs={12}>
+          <Paper className={classes.paper} />
+        </Grid>
+      </Grid>
     );
   }
 }
@@ -24,4 +64,4 @@ Index.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withRoot(withStyles(styles)(Index));
+export default withStyles(styles)(Index);
